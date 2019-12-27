2d616d3219931e2bf06fdb484ff5065ef194979a
Alexis Do Nascimento <alexis.dona@gmail.com>
2019-12-27 19:25:06 -0300

    versión final

--- src/main/java/TestDeJuego.java
+++ src/main/java/TestDeJuego.java
@@ -2,8 +2,13 @@ public class TestDeJuego {
     public static void main (String [ ] args) {
         //Instancio el Juego
         FizzBuzz juego = new FizzBuzz();
-        // Le pido al juego crear un arreglo de enteros de 150 posiciones
+        // Creo dos arreglos, uno de enteros y otro de Strings de enteros de 150 posiciones
         int [] arregloInts = new int[150];
+        //lleno el arreglo de enteros de 1 a 150
+        for (int i=0;i<arregloInts.length;i++) {
+            arregloInts[i] = i+1;
+        }
+
         String [] arregloDeStrings = new String[150];
 
         //Ejecuto el juego pasándole como parametro el arreglo instanciado y veo qué devuelve por consola.
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -12,26 +12,35 @@ public class FizzBuzzTest {
        this.arregloDeEnteros= new int[150];
        this.arregloDeStrings = new String[150];
        this.juego = new FizzBuzz();
+       for(int i=0;i<arregloDeEnteros.length;i++) {
+           arregloDeEnteros[i]=i+1;
+       }
+
+
   }
 
     @Test
     public void probarMultiploDeTres() {
-        int unNumero = 3;
+        int unNumero = arregloDeEnteros[2];
+        System.out.println("El numero en la posición 2 del arreglo es un : "+arregloDeEnteros[2]+" --> retorna "+this.juego.evaluarItem(arregloDeEnteros[2]));
         Assert.assertEquals(this.juego.evaluarItem(unNumero),"Fizz");
     }
     @Test
     public void probarMultiploDeCinco() {
-        int unNumero = 5;
+        int unNumero = arregloDeEnteros[4];
+        System.out.println("El numero en la posición 4 del arreglo es un : "+arregloDeEnteros[4]+" --> retorna "+this.juego.evaluarItem(arregloDeEnteros[4]));
         Assert.assertEquals(this.juego.evaluarItem(unNumero),"Buzz");
     }
     @Test
     public void probarMultiploDeTresYCinco() {
-        int unNumero = 15;
+        int unNumero = arregloDeEnteros[14];
+        System.out.println("El numero en la posición 14 del arreglo es un : "+arregloDeEnteros[14]+" --> retorna "+this.juego.evaluarItem(arregloDeEnteros[14]));
         Assert.assertEquals(this.juego.evaluarItem(unNumero),"FizzBuzz");
     }
     @Test
     public void probarNoEsMultiploDeTresYCinco() {
-        int unNumero = 16;
+        int unNumero = arregloDeEnteros[15];
+        System.out.println("El numero en la posición 3 del arreglo es un : "+arregloDeEnteros[15]+" --> retorna "+this.juego.evaluarItem(arregloDeEnteros[15]));
         Assert.assertEquals(this.juego.evaluarItem(unNumero),"16");
     }
 
